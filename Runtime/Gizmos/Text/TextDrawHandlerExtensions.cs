9b9a52552f3d2d688a01cc8b5676ced4d7882f4d
Demexis <devsor@inbox.lv>
2025-02-28 16:28:24 +0200

    refactor: moved Text API methods to extension-methods. Moved TextGizmo type to a separate file. Removed commented unused code.
    docs: Added XML-documentation to DebugXTextSettings.

---
+++ Runtime/Gizmos/Text/TextDrawHandlerExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace DCFApixels {
+    using IN = MethodImplAttribute;
+    using DrawHandler = DebugX.DrawHandler;
+    
+    public static class TextDrawHandlerExtensions {
+        private const MethodImplOptions LINE = DebugX.LINE;
+        
+        [IN(LINE)] public static DrawHandler Text(this DrawHandler drawHandler, Vector3 position, object text) =>
+            drawHandler.Gizmo(new TextGizmo(position, text, DebugXTextSettings.Default));
+
+        [IN(LINE)] public static DrawHandler Text(this DrawHandler drawHandler, Vector3 position, object text, DebugXTextSettings settings) => 
+            drawHandler.Gizmo(new TextGizmo(position, text, settings));
+    }
+}
