8308c622af44afdd5c75f4db0ce1bb0465e646d0
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2025-02-28 12:45:28 +0800

    update sample

--- Samples/Scripts/DebugXSample_Other.cs
+++ Samples/Scripts/DebugXSample_Other.cs
@@ -7,6 +7,7 @@ namespace DCFApixels.DebugXCore.Samples
     {
         public Gradient Gradient;
         public float GradientMultiplier = 5;
+        public Color TextBackgroundColor = Color.white;
         public Transform[] Points;
 
 #if UNITY_EDITOR
@@ -28,9 +29,7 @@ namespace DCFApixels.DebugXCore.Samples
             i++; DebugX.Draw(GetColor(Points[i])).Cross(Points[i].position, Points[i].localScale.x);
             i++; DebugX.Draw(GetColor(Points[i])).BillboardCircle(Points[i].position, Points[i].localScale.x * RADIUS_M);
             i++; DebugX.Draw(GetColor(Points[i])).WireMesh<SphereMesh>(Points[i].position, Points[i].rotation, Points[i].localScale * RADIUS_M);
-            Color backgroundColor = Color.white - GetColor(Points[i]);
-            backgroundColor.a = 0.5f;
-            i++; DebugX.Draw(GetColor(Points[i])).Text(Points[i].position, Points[i].name, DebugXTextSettings.WorldSpaceScale.SetSize(26).SetBackground(backgroundColor));
+            i++; DebugX.Draw(GetColor(Points[i])).Text(Points[i].position, Points[i].name, DebugXTextSettings.WorldSpaceScale.SetSize(26).SetBackground(TextBackgroundColor));
 
             i++; DebugX.Draw(GetColor(Points[i])).Dot(Points[i].position);
             i++; DebugX.Draw(GetColor(Points[i])).WireDot(Points[i].position);
