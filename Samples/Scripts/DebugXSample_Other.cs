e41c45d73f8887e84b9a71815e951c5c3922c7a4
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2025-02-28 11:20:58 +0800

    fix

--- Samples/Scripts/DebugXSample_Other.cs
+++ Samples/Scripts/DebugXSample_Other.cs
@@ -29,7 +29,6 @@ namespace DCFApixels.DebugXCore.Samples
             i++; DebugX.Draw(GetColor(Points[i])).BillboardCircle(Points[i].position, Points[i].localScale.x * RADIUS_M);
             i++; DebugX.Draw(GetColor(Points[i])).WireMesh<SphereMesh>(Points[i].position, Points[i].rotation, Points[i].localScale * RADIUS_M);
             Color backgroundColor = Color.white - GetColor(Points[i]);
-            //backgroundColor.a = 1;
             backgroundColor.a = 0.5f;
             i++; DebugX.Draw(GetColor(Points[i])).Text(Points[i].position, Points[i].name, DebugXTextSettings.Default.SetBackground(backgroundColor));
 
