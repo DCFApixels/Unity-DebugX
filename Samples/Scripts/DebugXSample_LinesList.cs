ecb40e4fb9c0f178c1dd7d45120a98243a348f7a
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2025-03-05 15:42:27 +0800

    update sample

--- Samples/Scripts/DebugXSample_LinesList.cs
+++ Samples/Scripts/DebugXSample_LinesList.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace DCFApixels.DebugXCore.Samples
@@ -7,7 +6,8 @@ namespace DCFApixels.DebugXCore.Samples
     public class DebugXSample_LinesList : MonoBehaviour
     {
         public Color Color;
-        public Transform[] Points;
+        public Vector3[] Points = new Vector3[32];
+        public float Frequency = 1;
         public bool IsStrip;
 
 #if UNITY_EDITOR
@@ -22,24 +22,27 @@ namespace DCFApixels.DebugXCore.Samples
         }
 #endif
 
-        private Vector3[] _convertedPoints = new Vector3[32];
         private void Draw()
         {
-            if(Points.Length > _convertedPoints.Length)
-            {
-                _convertedPoints = new Vector3[Points.Length << 1];
-            }
+            if (Points == null) { return; }
+            float sizeX = transform.localScale.x;
+            float sizeY = transform.localScale.y * 0.5f;
+
+            float start = transform.position.x - sizeX * 0.5f;
+            float step = sizeX / Points.Length;
             for (int i = 0; i < Points.Length; i++)
             {
-                _convertedPoints[i] = Points[i] == null ? Vector3.zero : Points[i].position;
+                float x = step * i;
+                float y = Mathf.Sin(x * Frequency) * sizeY;
+                Points[i] = new Vector3(start + x, transform.position.y + y, transform.position.z);
             }
             if (IsStrip)
             {
-                DebugX.Draw(Color).LineStrip(_convertedPoints, Points.Length);
+                DebugX.Draw(Color).LineStrip(Points, 0, Points.Length);
             }
             else
             {
-                DebugX.Draw(Color).Lines(_convertedPoints, Points.Length);
+                DebugX.Draw(Color).Lines(Points, 0, Points.Length);
             }
         }
     }
