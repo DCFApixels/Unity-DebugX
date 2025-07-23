9695037e09985d570aaa2dbc5561856931b6ff9a
DCFApixels <99481254+DCFApixels@users.noreply.github.com>
2025-07-23 19:13:52 +0800

    add ColorX

---
+++ Runtime/Utils/ColorX.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+namespace DCFApixels.DebugXCore
+{
+    public class ColorX
+    {
+        /// <summary> color code Red. RGB is (255, 0, 0)</summary>
+        public static readonly Color Red = new Color32(255, 0, 0, 255);
+        /// <summary> color code Green. RGB is (0, 255, 0)</summary>
+        public static readonly Color Green = new Color32(0, 255, 0, 255);
+        /// <summary> color code Blue. RGB is (0, 0, 255)</summary>
+        public static readonly Color Blue = new Color32(0, 0, 255, 255);
+        /// <summary> color code Yellow. RGB is (255, 255, 0)</summary>
+        public static readonly Color Yellow = new Color32(255, 255, 0, 255);
+        /// <summary> color code Cyan. RGB is (0, 255, 255)</summary>
+        public static readonly Color Cyan = new Color32(0, 255, 255, 255);
+        /// <summary> color code Magenta. RGB is (255, 0, 255)</summary>
+        public static readonly Color Magenta = new Color32(255, 0, 255, 255);
+        /// <summary> color code White. RGB is (255, 255, 255)</summary>
+        public static readonly Color White = new Color32(255, 255, 255, 255);
+        /// <summary> color code Black. RGB is (0, 0, 0)</summary>
+        public static readonly Color Black = new Color32(0, 0, 0, 255);
+
+        /// <summary> color code OrangeRed. RGB is (255, 69, 0)</summary>
+        public static readonly Color OrangeRed = new Color32(255, 69, 0, 255);
+        /// <summary> color code Orange. RGB is (255, 165, 0)</summary>
+        public static readonly Color Orange = new Color32(255, 165, 0, 255);
+        /// <summary> color code Purple. RGB is (128, 0, 128)</summary>
+        public static readonly Color Purple = new Color32(128, 0, 128, 255);
+        /// <summary> color code Pink. RGB is (255, 192, 203)</summary>
+        public static readonly Color Pink = new Color32(255, 192, 203, 255);
+        /// <summary> color code Brown. RGB is (165, 42, 42)</summary>
+        public static readonly Color Brown = new Color32(165, 42, 42, 255);
+        /// <summary> color code Grey/Gray. RGB is (128, 128, 128)</summary>
+        public static readonly Color Gray = new Color32(128, 128, 128, 255);
+        /// <summary> color code Grey/Gray. RGB is (128, 128, 128)</summary>
+        public static readonly Color Grey = Gray;
+        /// <summary> color code LightGray. RGB is (211, 211, 211)</summary>
+        public static readonly Color LightGray = new Color32(211, 211, 211, 255);
+        /// <summary> color code DarkGray. RGB is (64, 64, 64)</summary>
+        public static readonly Color DarkGray = new Color32(64, 64, 64, 255);
+        /// <summary> color code Lime. RGB is (125, 255, 0)</summary>
+        public static readonly Color Lime = new Color32(125, 255, 0, 255);
+        /// <summary> color code Teal. RGB is (0, 128, 128)</summary>
+        public static readonly Color Teal = new Color32(0, 128, 128, 255);
+        /// <summary> color code Olive. RGB is (128, 128, 0)</summary>
+        public static readonly Color Olive = new Color32(128, 128, 0, 255);
+        /// <summary> color code Navy. RGB is (0, 0, 128)</summary>
+        public static readonly Color Navy = new Color32(0, 0, 128, 255);
+        /// <summary> color code Maroon. RGB is (128, 0, 0)</summary>
+        public static readonly Color Maroon = new Color32(128, 0, 0, 255);
+        /// <summary> color code Aquamarine. RGB is (127, 255, 212)</summary>
+        public static readonly Color Aquamarine = new Color32(127, 255, 212, 255);
+        /// <summary> color code Fuchsia. RGB is (255, 0, 255)</summary>
+        public static readonly Color Fuchsia = new Color32(255, 0, 255, 255);
+        /// <summary> color code Silver. RGB is (192, 192, 192)</summary>
+        public static readonly Color Silver = new Color32(192, 192, 192, 255);
+        /// <summary> color code Gold. RGB is (255, 215, 0)</summary>
+        public static readonly Color Gold = new Color32(255, 215, 0, 255);
+        /// <summary> color code Indigo. RGB is (75, 0, 130)</summary>
+        public static readonly Color Indigo = new Color32(75, 0, 130, 255);
+        /// <summary> color code Violet. RGB is (238, 130, 238)</summary>
+        public static readonly Color Violet = new Color32(238, 130, 238, 255);
+        /// <summary> color code Coral. RGB is (255, 127, 80)</summary>
+        public static readonly Color Coral = new Color32(255, 127, 80, 255);
+        /// <summary> color code Salmon. RGB is (250, 128, 114)</summary>
+        public static readonly Color Salmon = new Color32(250, 128, 114, 255);
+        /// <summary> color code Turquoise. RGB is (64, 224, 208)</summary>
+        public static readonly Color Turquoise = new Color32(64, 224, 208, 255);
+        /// <summary> color code SkyBlue. RGB is (135, 206, 235)</summary>
+        public static readonly Color SkyBlue = new Color32(135, 206, 235, 255);
+        /// <summary> color code Plum. RGB is (221, 160, 221)</summary>
+        public static readonly Color Plum = new Color32(221, 160, 221, 255);
+        /// <summary> color code Khaki. RGB is (213, 197, 138)</summary>
+        public static readonly Color Khaki = new Color32(213, 197, 138, 255);
+        /// <summary> color code Beige. RGB is (237, 232, 208)</summary>
+        public static readonly Color Beige = new Color32(237, 232, 208, 255);
+        /// <summary> color code Lavender. RGB is (230, 230, 250)</summary>
+        public static readonly Color Lavender = new Color32(230, 230, 250, 255);
+        /// <summary> color code Goldenrod. RGB is (218, 165, 32)</summary>
+        public static readonly Color Goldenrod = new Color32(218, 165, 32, 255);
+        /// <summary> color code DeepPink. RGB is (255, 105, 180)</summary>
+        public static readonly Color DeepPink = new Color32(255, 105, 180, 255);
+        /// <summary> color code Crimson. RGB is (220, 20, 60)</summary>
+        public static readonly Color Crimson = new Color32(220, 20, 60, 255);
+        /// <summary> color code BlueViolet. RGB is (138, 43, 226)</summary>
+        public static readonly Color BlueViolet = new Color32(138, 43, 226, 255);
+        /// <summary> color code AmericanRose. RGB is (255, 3, 62)</summary>
+        public static readonly Color AmericanRose = new Color32(255, 3, 62, 255);
+    }
+}
